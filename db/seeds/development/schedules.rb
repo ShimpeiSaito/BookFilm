2b51f2db61536bedb4e35ea6a541eebfd15040a7
ShimpeiSaito <ne201103@senshu-u.jp>
2022-01-23 15:46:44 +0000

    herokuのための調整

--- db/seeds/development/schedules.rb
+++ db/seeds/development/schedules.rb
@@ -1,20 +1,19 @@
-#times = %w(9:00 11:00 13:00 15:00 17:00 19:00 21:00)
-times = %w(9 11 13 15 17 19 21)
+times = %w(9 12 15 18)
 
 1.upto(3) do |thea_idx|
     theater = Theater.find(thea_idx)
     movies = Movie.all.sample(rand(13..20))
+    scn = [1,2,3,4,5,6,7,8,8,7,6,5,4,3,2,1]
     for movie in movies do
         0.upto(6) do |day|
             date = Date.new(Time.zone.now.year, Time.zone.now.month,(Time.zone.now.day) + day)
-            1.upto(3) do
-                #time = Time.zone.parse(times.sample)
-                time = Time.new(date.year, date.month, date.day, times.sample, 0, 0, '+09:00')
+            0.upto(3) do |t|
+                time = Time.new(date.year, date.month, date.day, times[t], 0, 0, '+09:00')
                 etime = time + (movie.screening_time * 60)
                 Schedule.create(
                     thea: theater,
                     mov: movie,
-                    screen_no: rand(1..8),
+                    screen_no: scn[t + day],
                     screening_date: date,
                     starttime: time,
                     endtime: etime
