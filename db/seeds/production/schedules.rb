69028636e8f8e3fafc74b58657d74b788ed840fe
ShimpeiSaito <ne201103@senshu-u.jp>
2022-01-26 10:30:22 +0000

    seedデータの修正

--- db/seeds/production/schedules.rb
+++ db/seeds/production/schedules.rb
@@ -6,7 +6,7 @@ times = %w(6 12 18 8 14 20 10 16 22)
     scn = [1,8,2,7,3,6,4,5]
     for movie in movies do
         0.upto(6) do |day|
-            date = Date.new(Time.zone.now.year, Time.zone.now.month,(Time.zone.now.day) + day)
+            date = Date.current + day
             0.upto(2) do |t|
                 time = Time.new(date.year, date.month, date.day, times[t + day], 0, 0, '+09:00')
                 etime = time + (movie.screening_time * 60)
